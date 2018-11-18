aedab0eeb87078829a7e111f492ec0102ce2c488
Zygy93 <amazinzygy@gmail.com>
2018-11-18 18:44:34 -0500

    added Auth

---
+++ src/modules/auth.js
@@ -0,0 +1,14 @@
+const jwt = require('express-jwt');
+const { sign } = require('jsonwebtoken');
+
+const secret = process.env.JWT_SECRET || 'secret';
+
+const options = {
+  secret
+};
+
+function signToken(userId) {
+  return sign({ userId, permissions: 'admin' }, secret);
+}
+
+module.exports = { jwt: jwt(options).unless({ path: ['/api/user', '/api/login'] }), signToken };
