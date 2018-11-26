2c310a571671332ed1e524113467d48e91cb681a
James Nguyen <33408161+Zygy93@users.noreply.github.com>
2018-11-26 11:51:54 -0500

    Update RegExp Object comment

--- src/modules/auth.js
+++ src/modules/auth.js
@@ -10,7 +10,9 @@ const options = {
 function signToken(userId) {
   return sign({ userId, permissions: 'admin' }, secret);
 }
-//Attaches the userId to the sign token and giving admin authorization
+// Attaches the userId to the sign token and giving admin authorization
 
 module.exports = { jwt: jwt(options).unless({ path: ['/api/user', '/api/login', /\/api\/user\/\d*/] }), signToken };
+// Regular expressions are used to perform pattern-matching and "search-and-replace" functions on text. IN this case,
+// We are finding the API UserId
 // Exporting the JWT
